81b86a85992ae54dc30f62f2d60a1d30bb8c7edf
Dmitriy/ <50639080+demos1981@users.noreply.github.com>
2025-08-14 23:01:49 +0300

    change: change components gamefild and rewardcounter

--- src/components/GameField.tsx
+++ src/components/GameField.tsx
@@ -2,9 +2,21 @@ import React from "react";
 
 const GameField: React.FC = () => {
   return (
-    <div>
-      <h2>Game Field</h2>
-    </div>
+    <>
+      <div className="flex justify-center items-center">
+        <div className="w-[356px] h-[356px] grid grid-cols-3 gap-2 mt-5">
+          <div className="bg-gray-200 h-[112px]"></div>
+          <div className="bg-gray-200 h-[112px]"></div>
+          <div className="bg-gray-200 h-[112px]"></div>
+          <div className="bg-gray-200 h-[112px]"></div>
+          <div className="bg-gray-200 h-[112px]"></div>
+          <div className="bg-gray-200 h-[112px]"></div>
+          <div className="bg-gray-200 h-[112px]"></div>
+          <div className="bg-gray-200 h-[112px]"></div>
+          <div className="bg-gray-200 h-[112px]"></div>
+        </div>
+      </div>
+    </>
   );
 };
 
--- src/components/RewardCounter.tsx
+++ src/components/RewardCounter.tsx
@@ -2,8 +2,15 @@ import React from "react";
 
 const RewardCounter: React.FC = () => {
   return (
-    <div>
-      <h2>Reward Counter</h2>
+    <div className="mt-5 w-[428px] flex flex-row justify-center items-center">
+      <img
+        className="h-[50px]"
+        src="src/assets/Cash.svg"
+        alt="Reward Counter"
+      />
+      <span className="w-[36px] h-[36px] text-indigo-50 font-extrabold text-4xl">
+        0
+      </span>
     </div>
   );
 };
